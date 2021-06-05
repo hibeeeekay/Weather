77c3a3ca97c341a69326cc6da876eb5d1cc13f21
hibeeeekay <60797412+hibeeeekay@users.noreply.github.com>
2021-06-05 22:18:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   }
 
   return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input 
